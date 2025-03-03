965a6c73370bf0938ccf7edd71d47a6ea367573e
MusyafaRT <89581886+MusyafaRT@users.noreply.github.com>
2025-03-03 21:24:31 +0700

    [feat] add num_classess args

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ def get_args_parser():
     # Model parameters
     parser.add_argument('--frozen_weights', type=str, default=None,
                         help="Path to the pretrained model. If set, only the mask head will be trained")
+    parser.add_argument('--num_classes', type=int, default=2, help='Jumlah kelas termasuk background')
+
     # * Backbone
     parser.add_argument('--backbone', default='resnet50', type=str,
                         help="Name of the convolutional backbone to use")
